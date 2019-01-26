6fbea2adab567f736c436d3b5a140beae97fee67
GavstarB <45681174+GavstarB@users.noreply.github.com>
2019-01-26 10:06:49 +0000

    added occupancy grid scripts

---
+++ Assets/Scripts/HousePart.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct HousePartType
+{
+    public float xsize;
+    public float ysize;
+    public float zsize;
+    public GameObject model;
+    public bool[] faces;
+}
+
+public class HousePart : MonoBehaviour
+{
+    public float xpos;
+    public float ypos;
+    public float zpos;
+    public HousePartType part;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
