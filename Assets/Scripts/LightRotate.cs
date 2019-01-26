5e9cadab06eeb29614cf997a4f9bdeebda9de85a
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2019-01-26 22:11:42 +0000

    Add lighthouse revolving light

---
+++ Assets/Scripts/LightRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightRotate : MonoBehaviour
+{
+    public float rpm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, Time.deltaTime * 100f);
+    }
+}
