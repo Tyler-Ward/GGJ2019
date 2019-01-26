79f74817df26ac1570fc0c107814e2ad0f8ee983
Tyler Ward <tyler@scorpia.co.uk>
2019-01-26 15:45:24 +0000

    Fixed issues with player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    public ResourceManager resources;
+
+    public float MaxSpeed = 2;
+    public float MaxTurnSpeed = 180f;
+    public float Aceleration = 1f;
+    public float TurnAceleration = 180;
+
+    private float CurrentSpeed = 0;
+    private float CurrentTurn = 0;
 
     void Start()
     {
@@ -13,13 +22,78 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        float moveVertical = Input.GetAxis("Vertical");
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        Move();
+        Turn();
+        return;
 
-        Vector3 acceleration = new Vector3(0.0f, 0.0f, moveVertical);
-        Vector3 rotation = new Vector3(0.0f, moveHorizontal, 0.0f);
+    }
 
-        rb.AddRelativeForce(acceleration * 10);
-        rb.AddTorque(rotation * 9);
+    private void Move()
+    {
+
+
+        if (resources.Fuel > 0 && Input.GetAxis("Vertical")!=0)
+        {
+            CurrentSpeed += Input.GetAxis("Vertical")* Aceleration* Time.deltaTime;
+        }
+        else
+        {
+            if (CurrentSpeed > 0)
+            {
+                CurrentSpeed -= (Aceleration * Time.deltaTime / 2);
+                if (CurrentSpeed < 0)
+                    CurrentSpeed = 0;
+            }
+            else if (CurrentSpeed < 0)
+            {
+                CurrentSpeed += (Aceleration * Time.deltaTime / 2);
+                if (CurrentSpeed > 0)
+                    CurrentSpeed = 0;
+            }
+        }
+
+        if (CurrentSpeed > MaxSpeed)
+            CurrentSpeed = MaxSpeed;
+        if (CurrentSpeed < -MaxSpeed)
+            CurrentSpeed = -MaxSpeed;
+
+        Vector3 movement = transform.forward * CurrentSpeed * Time.deltaTime;
+        rb.MovePosition(rb.position + movement);
+    }
+
+    private void Turn()
+    {
+        if (resources.Fuel > 0 && Input.GetAxis("Horizontal") != 0)
+        {
+            CurrentTurn += Input.GetAxis("Horizontal") * TurnAceleration * Time.deltaTime;
+        }
+        else
+        {
+            if (CurrentTurn > 0)
+            {
+                CurrentTurn -= (TurnAceleration * Time.deltaTime / 1.5f);
+                if (CurrentTurn < 0)
+                    CurrentTurn = 0;
+            }
+            else if (CurrentTurn < 0)
+            {
+                CurrentTurn += (TurnAceleration * Time.deltaTime / 1.5f);
+                if (CurrentTurn > 0)
+                    CurrentTurn = 0;
+            }
+        }
+
+        if (CurrentTurn > MaxTurnSpeed)
+            CurrentTurn = MaxTurnSpeed;
+        if (CurrentTurn < -MaxTurnSpeed)
+            CurrentTurn = -MaxTurnSpeed;
+
+
+
+        // Determine the number of degrees to be turned based on the input, speed and time between frames.
+        float turn = CurrentTurn * Time.deltaTime;
+
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+        rb.MoveRotation(rb.rotation * turnRotation);
     }
 }
