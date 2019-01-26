95bdb3bc5e8881ea21e8d53f0f84ab6eb9a02a92
GavstarB <45681174+GavstarB@users.noreply.github.com>
2019-01-26 19:49:54 +0000

    Added Procedural Map

--- Assets/Scripts/NoiseMap.cs
+++ Assets/Scripts/NoiseMap.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class NoiseMap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int mapWidth;
+    public int mapHeight;
+    public int seed;
+    public float scale;
+    public int octaves;
+    public float persistence;
+    public float lacunarity;
+    public Vector2 offset;
+    public float height;
+    public Material grass;
+
+    private void CreateMesh(float[,] heightmap)
     {
-        
+        Mesh mesh = new Mesh();
+        List<Vector3> vertices = new List<Vector3>();
+        List<Vector2> uvs = new List<Vector2>();
+        List<int> triangles = new List<int>();
+
+        for (int x = 0; x < mapWidth; x++)
+        {
+            for (int y = 0; y < mapHeight; y++)
+            {
+                vertices.Add(new Vector3(x, heightmap[x, y] * height, y));
+                uvs.Add(new Vector2(0.1f * (mapWidth - x), 0.1f * (mapHeight - y)));
+            }
+        }
+        for (int x = 0; x < mapWidth - 1; x++)
+        {
+            for (int y = 0; y < mapHeight - 1; y++)
+            {
+                triangles.Add(y * mapWidth + x);
+                triangles.Add(y * mapWidth + x + 1);
+                triangles.Add(y * mapWidth + x + mapWidth);
+
+                triangles.Add(y * mapWidth + x + mapWidth + 1);
+                triangles.Add(y * mapWidth + x + mapWidth);
+                triangles.Add(y * mapWidth + x + 1);
+            }
+        }
+
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.uv = uvs.ToArray();
+        mesh.RecalculateNormals();
+
+        gameObject.GetComponent<MeshFilter>().mesh = mesh;
+        gameObject.GetComponent<MeshCollider>().sharedMesh = gameObject.GetComponent<MeshFilter>().mesh;
+        gameObject.GetComponent<MeshRenderer>().material = grass;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    void Awake()
     {
-        
+        gameObject.AddComponent<MeshRenderer>();
+        gameObject.AddComponent<MeshFilter>();
+        //gameObject.AddComponent<MeshCollider>();
+
+        Noise noise = new Noise();
+        float[,] heightmap = noise.PerlinNoiseMap2D(mapWidth, mapHeight, seed, scale, octaves, persistence, lacunarity, offset);
+        CreateMesh(heightmap);
     }
 }
