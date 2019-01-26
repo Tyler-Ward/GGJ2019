16b0518e20d4a9f2229d3dd46a053b36567781a7
GavstarB <45681174+GavstarB@users.noreply.github.com>
2019-01-26 16:47:09 +0000

    tree felling

--- Assets/Scripts/TreeFeller.cs
+++ Assets/Scripts/TreeFeller.cs
@@ -6,6 +6,9 @@ public class TreeFeller : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-
+        if(gameObject.GetComponent<Rigidbody>() == null)
+        {
+            gameObject.AddComponent<Rigidbody>();
+        }
     }
 }
